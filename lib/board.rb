44c9fe4c25eb8c66ea7ab13373e4709e34ba47a1
QuentinTHP <quentin.clere.pro@gmail.com>
2019-01-24 14:51:26 +0100

    on start le board

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,7 @@
-
+require 'boardcase'
 
 class Board
+  attr_accessor :boardcase_array_hash
     #TO DO : la classe a 1 attr_accessor : un array/hash qui contient les BoardCases.
     #Optionnellement on peut aussi lui rajouter un autre sous le nom @count_turn pour compter le nombre de coups joué
   
@@ -9,6 +10,18 @@ class Board
       #TO DO :
       #Quand la classe s'initialize, elle doit créer 9 instances BoardCases
       #Ces instances sont rangées dans un array/hash qui est l'attr_accessor de la classe
+      puts "-" * 15
+      puts "|#{a1 = BoardCase.new}|
+      |#{a2 = BoardCase.new}|
+      |#{a3 = BoardCase.new}|"/n
+      puts "|#{b1 = BoardCase.new}|
+      |#{b2 = BoardCase.new}|
+      |#{b3 = BoardCase.new}|"/n
+      puts "|#{c1 = BoardCase.new}|
+      |#{c2 = BoardCase.new}|
+      |#{c3 = BoardCase.new}|"
+      puts "-" * 15
+
     end
   
     def play_turn
--- lib/boardcase.rb
+++ lib/boardcase.rb
@@ -2,10 +2,13 @@
 
 class BoardCase
     #TO DO : la classe a 2 attr_accessor, sa valeur en string (X, O, ou vide), ainsi que son identifiant de case
+    attr_accessor :string_symbol, :place
   
     
-    def initialize
+    def initialize(string_symbol, place)
       #TO DO : doit régler sa valeur, ainsi que son numéro de case
+      @string_symbol = name
+      @place = symbol
     end
     
-  end
+end
